c5488e7f7dc7efb346d255d4fab1faa3f4c638e0
Dgrady3 <joegrady1@gmail.com>
2014-10-05 00:08:46 -0400

    another spacing tweak

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,7 @@
 class Car < ActiveRecord::Base
   belongs_to :manufacturer
 
-  validates :manufacturer_id, presence: true
-  validates :color, presence: true
+  validates :manufacturer_id, :color, presence: true
   validates :mileage, numericality: { only_integer: true }
   validates :year, numericality:  { greater_than_or_equal_to: 1920 }
 end
