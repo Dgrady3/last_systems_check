a5a3ba4a25efd0c92e8cded8e504dc6be3a78e97
Helen Hood <helen.c.hood@gmail.com>
2014-10-07 14:32:02 -0400

    Rename and modify migrations

--- db/migrate/20141004022912_create_manufacturers.rb
+++ db/migrate/20141004022912_create_manufacturers.rb
@@ -1,8 +1,10 @@
-class Manufacturer < ActiveRecord::Migration
+class CreateManufacturers < ActiveRecord::Migration
   def change
     create_table :manufacturers do |t|
       t.string :name, null: false
       t.string :country, null: false
     end
+
+    add_index :manufacturers, :name, unique: true
   end
 end
